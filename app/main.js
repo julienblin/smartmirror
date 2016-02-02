eedcf13d116547592f2ee08b57a3c294271cdce4
Julien Blin <julien.blin@gmail.com>
2016-02-02 18:27:06 -0500

    Added angular app + fullscreen launch.

--- app/main.js
+++ app/main.js
@@ -6,8 +6,13 @@ var BrowserWindow = require('browser-window');
 var mainWindow = null;
 
 app.on('ready', function () {
-  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  mainWindow = new BrowserWindow({ fullscreen: true });
   mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  // Open the DevTools if run with "--debug as argument"
+  if(process.argv[1].search("debug") != -1){
+    mainWindow.webContents.openDevTools();
+  }
 
   mainWindow.on('closed', function () {
     mainWindow = null;
